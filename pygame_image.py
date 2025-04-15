dbaae88a2404c9b0cd44ee1c9816c8b5302620bb
c0a240021c <c0a240021c@edu.teu.ac.jp>
2025-04-15 17:22:43 +0900

    課題１

--- pygame_image.py
+++ pygame_image.py
@@ -30,8 +30,9 @@ def main():
         if key_lst[pg.K_LEFT]:
             kk_rct.move_ip((-1,0))
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((1,0))
+            kk_rct.move_ip((2,0))
 
+        kk_rct.move_ip((-1,0))
         
         x = tmr % 3200 #練習9
         
